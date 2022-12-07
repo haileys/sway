7bdafe838c8481820d1ddc278541d9bb63a98ee9
Hailey Somerville <hailey@hailey.lol>
2022-12-07 14:11:50 +1100

    fix race in swaybar

--- swaybar/status_line.c
+++ swaybar/status_line.c
@@ -34,104 +34,120 @@ void status_error(struct status_line *status, const char *text) {
 	status->text = text;
 }
 
+static bool status_handle_first_read(struct status_line *status) {
+	errno = 0;
+	int available_bytes;
+	if (ioctl(status->read_fd, FIONREAD, &available_bytes) == -1) {
+		sway_log(SWAY_ERROR, "Unable to read status command output size");
+		status_error(status, "[error reading from status command]");
+		return true;
+	}
+
+	if ((size_t)available_bytes + 1 > status->buffer_size) {
+		// need room for leading '\0' too
+		status->buffer_size = available_bytes + 1;
+		status->buffer = realloc(status->buffer, status->buffer_size);
+	}
+	if (status->buffer == NULL) {
+		sway_log_errno(SWAY_ERROR, "Unable to read status line");
+		status_error(status, "[error reading from status command]");
+		return true;
+	}
+
+	ssize_t read_bytes = read(status->read_fd, status->buffer, available_bytes);
+	if (read_bytes != available_bytes) {
+		status_error(status, "[error reading from status command]");
+		return true;
+	}
+	status->buffer[available_bytes] = 0;
+
+	// the header must be sent completely the first time round
+	char *newline = strchr(status->buffer, '\n');
+	json_object *header, *version;
+	if (newline != NULL
+			&& (header = json_tokener_parse(status->buffer))
+			&& json_object_object_get_ex(header, "version", &version)
+			&& json_object_get_int(version) == 1) {
+		sway_log(SWAY_DEBUG, "Using i3bar protocol.");
+		status->protocol = PROTOCOL_I3BAR;
+
+		json_object *click_events;
+		if (json_object_object_get_ex(header, "click_events", &click_events)
+				&& json_object_get_boolean(click_events)) {
+			sway_log(SWAY_DEBUG, "Enabling click events.");
+			status->click_events = true;
+			if (write(status->write_fd, "[\n", 2) != 2) {
+				status_error(status, "[failed to write to status command]");
+				json_object_put(header);
+				return true;
+			}
+		}
+
+		json_object *float_event_coords;
+		if (json_object_object_get_ex(header, "float_event_coords", &float_event_coords)
+				&& json_object_get_boolean(float_event_coords)) {
+			sway_log(SWAY_DEBUG, "Enabling floating-point coordinates.");
+			status->float_event_coords = true;
+		}
+
+		json_object *signal;
+		if (json_object_object_get_ex(header, "stop_signal", &signal)) {
+			status->stop_signal = json_object_get_int(signal);
+			sway_log(SWAY_DEBUG, "Setting stop signal to %d", status->stop_signal);
+		}
+		if (json_object_object_get_ex(header, "cont_signal", &signal)) {
+			status->cont_signal = json_object_get_int(signal);
+			sway_log(SWAY_DEBUG, "Setting cont signal to %d", status->cont_signal);
+		}
+
+		json_object_put(header);
+
+		wl_list_init(&status->blocks);
+		status->tokener = json_tokener_new();
+		status->buffer_index = strlen(newline + 1);
+		memmove(status->buffer, newline + 1, status->buffer_index + 1);
+		return i3bar_handle_readable(status);
+	}
+
+	sway_log(SWAY_DEBUG, "Using text protocol.");
+	status->protocol = PROTOCOL_TEXT;
+
+	if (status->buffer[read_bytes - 1] == '\n') {
+		status->buffer[read_bytes - 1] = '\0';
+	}
+
+	const char* last_newline = strrchr(status->buffer, '\n');
+	if (last_newline) {
+		status->text = last_newline + 1;
+	} else {
+		status->text = status->buffer;
+	}
+
+	return true;
+}
+
+static bool text_handle_readable(struct status_line *status)  {
+	while (true) {
+		errno = 0;
+		ssize_t read_bytes = getline(&status->buffer,
+				&status->buffer_size, status->read);
+		if (errno == EAGAIN) {
+			status->text = status->buffer;
+			clearerr(status->read);
+			return true;
+		} else if (errno) {
+			status_error(status, "[error reading from status command]");
+			return true;
+		}
+	}
+}
+
 bool status_handle_readable(struct status_line *status) {
-	ssize_t read_bytes = 1;
 	switch (status->protocol) {
 	case PROTOCOL_UNDEF:
-		errno = 0;
-		int available_bytes;
-		if (ioctl(status->read_fd, FIONREAD, &available_bytes) == -1) {
-			sway_log(SWAY_ERROR, "Unable to read status command output size");
-			status_error(status, "[error reading from status command]");
-			return true;
-		}
-
-		if ((size_t)available_bytes + 1 > status->buffer_size) {
-			// need room for leading '\0' too
-			status->buffer_size = available_bytes + 1;
-			status->buffer = realloc(status->buffer, status->buffer_size);
-		}
-		if (status->buffer == NULL) {
-			sway_log_errno(SWAY_ERROR, "Unable to read status line");
-			status_error(status, "[error reading from status command]");
-			return true;
-		}
-
-		read_bytes = read(status->read_fd, status->buffer, available_bytes);
-		if (read_bytes != available_bytes) {
-			status_error(status, "[error reading from status command]");
-			return true;
-		}
-		status->buffer[available_bytes] = 0;
-
-		// the header must be sent completely the first time round
-		char *newline = strchr(status->buffer, '\n');
-		json_object *header, *version;
-		if (newline != NULL
-				&& (header = json_tokener_parse(status->buffer))
-				&& json_object_object_get_ex(header, "version", &version)
-				&& json_object_get_int(version) == 1) {
-			sway_log(SWAY_DEBUG, "Using i3bar protocol.");
-			status->protocol = PROTOCOL_I3BAR;
-
-			json_object *click_events;
-			if (json_object_object_get_ex(header, "click_events", &click_events)
-					&& json_object_get_boolean(click_events)) {
-				sway_log(SWAY_DEBUG, "Enabling click events.");
-				status->click_events = true;
-				if (write(status->write_fd, "[\n", 2) != 2) {
-					status_error(status, "[failed to write to status command]");
-					json_object_put(header);
-					return true;
-				}
-			}
-
-			json_object *float_event_coords;
-			if (json_object_object_get_ex(header, "float_event_coords", &float_event_coords)
-					&& json_object_get_boolean(float_event_coords)) {
-				sway_log(SWAY_DEBUG, "Enabling floating-point coordinates.");
-				status->float_event_coords = true;
-			}
-
-			json_object *signal;
-			if (json_object_object_get_ex(header, "stop_signal", &signal)) {
-				status->stop_signal = json_object_get_int(signal);
-				sway_log(SWAY_DEBUG, "Setting stop signal to %d", status->stop_signal);
-			}
-			if (json_object_object_get_ex(header, "cont_signal", &signal)) {
-				status->cont_signal = json_object_get_int(signal);
-				sway_log(SWAY_DEBUG, "Setting cont signal to %d", status->cont_signal);
-			}
-
-			json_object_put(header);
-
-			wl_list_init(&status->blocks);
-			status->tokener = json_tokener_new();
-			status->buffer_index = strlen(newline + 1);
-			memmove(status->buffer, newline + 1, status->buffer_index + 1);
-			return i3bar_handle_readable(status);
-		}
-
-		sway_log(SWAY_DEBUG, "Using text protocol.");
-		status->protocol = PROTOCOL_TEXT;
-		status->text = status->buffer;
-		// intentional fall-through
+		return status_handle_first_read(status);
 	case PROTOCOL_TEXT:
-		while (true) {
-			if (status->buffer[read_bytes - 1] == '\n') {
-				status->buffer[read_bytes - 1] = '\0';
-			}
-			errno = 0;
-			read_bytes = getline(&status->buffer,
-					&status->buffer_size, status->read);
-			if (errno == EAGAIN) {
-				clearerr(status->read);
-				return true;
-			} else if (errno) {
-				status_error(status, "[error reading from status command]");
-				return true;
-			}
-		}
+		return text_handle_readable(status);
 	case PROTOCOL_I3BAR:
 		return i3bar_handle_readable(status);
 	default:
